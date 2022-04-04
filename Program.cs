52189f2018512a0251ee77954be660403d77df95
4Karolis <4karolis.t@gmail.com>
2022-04-04 22:06:56 +0300

    created

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Library;
+using System;
+
+namespace Tema_08___BIBLIOTEKA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Super Book", "123456");
+
+            // galim uzsettit per SET dali
+            book.Name = "Some name"; // cia neleidiz settint nauju value, nes darasem PRIVATE kitam lange
+            book.id = "54364656";
+
+            //gaunam per GET dali
+            Console.WriteLine(book.Name);
+            Console.WriteLine(book.id);
+        }
+    }
+}
