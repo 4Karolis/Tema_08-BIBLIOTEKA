1d9d352955ae7dcda8db09d9905d4e475e8d4c29
4Karolis <4karolis.t@gmail.com>
2022-04-07 02:10:18 +0300

    sth

--- Saskaita.cs
+++ Saskaita.cs
@@ -6,25 +6,25 @@ namespace Tema_08___BIBLIOTEKA
 {
     public struct Saskaita
     {
-        private string _siuntejas;
-        private string _gavejas;
-        private decimal _moketiViso;
-        public string SaskaitosNumeris;
+        //private string _siuntejas;
+        //private string _gavejas;
+        //private decimal _moketiViso;
+        //public string SaskaitosNumeris;
 
-        public Saskaita(string siuntejas, string gavejas, decimal moketiViso)
-        {
-            _siuntejas = siuntejas;
-            _gavejas = gavejas;
-            _moketiViso = moketiViso;
-            SaskaitosNumeris = string.Empty;
-            SaskaitosNumeris = KurtiSaskaitosNumeri();
-        }
+        //public Saskaita(string siuntejas, string gavejas, decimal moketiViso)
+        //{
+        //    _siuntejas = siuntejas;
+        //    _gavejas = gavejas;
+        //    _moketiViso = moketiViso;
+        //    SaskaitosNumeris = string.Empty;
+        //    KurtiSaskaitosNumeri();
+        //}
 
-        public string SaskaitosNumeris { get; private set; }
+        //public string SaskaitosNumeris { get; private set; }
               
-        private void KurtiSaskaitosNumeri()
-        {
-            SaskaitosNumeris = string.Format("{}_{}_{}_{}_{}", //parametrai, );
-        }
+        //private void KurtiSaskaitosNumeri()
+        //{
+        //    SaskaitosNumeris = string.Format("{}_{}_{}_{}_{}", //parametrai, );
+        //}
     }
 }
